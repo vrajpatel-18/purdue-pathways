de1e24607d517d9a8cb750c5584df9deba91bba9
nshah067 <60299940+nshah067@users.noreply.github.com>
2024-03-30 13:15:10 -0400

    created chroma script

---
+++ pp-backend/query.py
@@ -0,0 +1,42 @@
+import argparse
+from dataclasses import dataclass
+from langchain.vectorstores.chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+def make_query(query_text:str):
+    # Prepare the DB.
+    embedding_function = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=5)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+
+    model = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    #print(formatted_response)
+    
+    return formatted_response
---
+++ pp-backend/vector_db.py
@@ -0,0 +1,58 @@
+from langchain_community.document_loaders.directory import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+import os
+import shutil
+import json
+
+CHROMA_PATH = "chroma"
+DATA_PATH = "data"
+
+def generate_data_store():
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+
+
+def load_documents():
+    loader = DirectoryLoader(DATA_PATH, glob="*.txt")
+    documents = loader.load()
+    return documents
+
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=750,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+        separators=[
+        "\n\n",
+        "\n",
+        " ",
+        ".",
+        ",",
+    ],
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    #document = chunks[10]
+    #print(document.page_content)
+    #print(document.metadata)
+
+    return chunks
+
+def save_to_chroma(chunks: list[Document]):
+    # Clear out the database first.
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Create a new DB from the documents.
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
