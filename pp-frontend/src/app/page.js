ae8d704d0eb331f093efa8a03138af69cce1b2f8
Siddhanth Kumar <53668449+sidgithub780@users.noreply.github.com>
2024-03-30 13:15:06 -0400

    landing page done

--- pp-frontend/src/app/page.js
+++ pp-frontend/src/app/page.js
@@ -4,7 +4,7 @@ import logo from "./pathways_transparent.png";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-[#212121]">
-      <header className="p-5">
+      <header className="p-2">
         <div className="flex items-center">
           <Image src={logo} alt="Pathways logo" width={80} height={60} />
           <span className="text-3xl font-bold text-[#e7b564] ml-3">
@@ -12,7 +12,7 @@ export default function Home() {
           </span>
         </div>
       </header>
-      <main className="flex-grow flex items-center justify-center">
+      <main className="flex-grow flex items-center justify-center mb-10">
         <div className="text-center mb-30">
           <h1 className="text-7xl font-bold text-[#e7b564] mb-3">
             Course planning, made easy
@@ -26,7 +26,13 @@ export default function Home() {
             Experience personalized scheduling, course selection, and academic
             advice right at your fingertips.
           </p>
-          test
+
+          <input
+            class="shadow appearance-none border rounded p-6 w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-5"
+            id="userPrompt"
+            type="text"
+            placeholder="let's forward your education..."
+          />
         </div>
       </main>
     </div>
