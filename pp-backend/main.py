d61d312cea5ce05f2953ffff248fefa1f1e408e1
nshah067 <60299940+nshah067@users.noreply.github.com>
2024-03-31 01:01:17 -0400

    more retrieval experimentation

--- pp-backend/main.py
+++ pp-backend/main.py
@@ -1,17 +1,16 @@
 from vector_db import generate_data_store
-from retrievers import similarity_search, reciprocal_rank_fusion
+from retrievers import similarity_search, reciprocal_rank_fusion, structured_retrieval
 
 # generate_data_store("pp-backend/chroma/basic", "pp-backend/data/basic")
 # generate_data_store("pp-backend/chroma/geneds", "pp-backend/data/geneds")
-# generate_data_store("pp-backend/chroma/track", "pp-backend/data/tracks")
+# generate_data_store("pp-backend/chroma/track", "pp-backend/data/track")
 
 generate_data_store()
+
 start = "y"
 while start == "y":
     question = input("Ask your pdf some question: ")
-    #print(similarity_search(question))
-    print(reciprocal_rank_fusion(question))
-    #print(colbert_retriever(question))
     #print(structured_retrieval(question))
+    print(reciprocal_rank_fusion(question))
     start = input("Would you like to keep chatting: ")
     
